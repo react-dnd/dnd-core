482e66ae9d0377440492135806f3dd2cffeec02d
Dan Abramov <dan.abramov@gmail.com>
2015-03-08 15:21:31 +0300

    Group tests

--- modules/__tests__/DragDropManager-test.js
+++ modules/__tests__/DragDropManager-test.js
@@ -62,203 +62,209 @@ describe('DragDropManager', () => {
   });
 
   describe('drag source and target contract', () => {
-    it('ignores beginDrag() if canDrag() returns false', () => {
-      const source = new NonDraggableSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
+    describe('beginDrag', () => {
+      it('ignores beginDrag() if canDrag() returns false', () => {
+        const source = new NonDraggableSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
 
-      backend.simulateBeginDrag(sourceHandle);
-      expect(source.didCallBeginDrag).to.equal(false);
+        backend.simulateBeginDrag(sourceHandle);
+        expect(source.didCallBeginDrag).to.equal(false);
+      });
+
+      it('throws if beginDrag() returns non-object', () => {
+        const source = new BadItemSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+
+        expect(() => backend.simulateBeginDrag(sourceHandle)).to.throwError();
+      });
+
+      it('begins drag if canDrag() returns true', () => {
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+
+        backend.simulateBeginDrag(sourceHandle);
+        expect(source.didCallBeginDrag).to.equal(true);
+      });
+
+      it('throws in beginDrag() if it is called twice during one operation', () => {
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+
+        backend.simulateBeginDrag(sourceHandle);
+        expect(() => backend.simulateBeginDrag(sourceHandle)).to.throwError();
+      });
+
+      it('lets beginDrag() be called again in a next operation', () => {
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+
+        backend.simulateBeginDrag(sourceHandle);
+        backend.simulateEndDrag(sourceHandle);
+
+        source.didCallBeginDrag = false;
+        expect(() => backend.simulateBeginDrag(sourceHandle)).to.not.throwError();
+        expect(source.didCallBeginDrag).to.equal(true);
+      });
     });
 
-    it('throws if beginDrag() returns non-object', () => {
-      const source = new BadItemSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
+    describe('drop() and endDrag()', () => {
+      it('endDrag() sees drop() return value as drop result if dropped on a target', () => {
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+        const target = new NormalTarget();
+        const targetHandle = registry.addTarget(Types.FOO, target);
 
-      expect(() => backend.simulateBeginDrag(sourceHandle)).to.throwError();
+        backend.simulateBeginDrag(sourceHandle);
+        backend.simulateEnter(targetHandle);
+        backend.simulateDrop();
+        backend.simulateEndDrag();
+        expect(target.didCallDrop).to.equal(true);
+        expect(source.recordedDropResult.foo).to.equal('bar');
+      });
+
+      it('endDrag() sees true as drop result by default if dropped on a target', () => {
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+        const target = new TargetWithNoDropResult();
+        const targetHandle = registry.addTarget(Types.FOO, target);
+
+        backend.simulateBeginDrag(sourceHandle);
+        backend.simulateEnter(targetHandle);
+        backend.simulateDrop();
+        backend.simulateEndDrag();
+        expect(source.recordedDropResult).to.equal(true);
+      });
+
+      it('endDrag() sees false as drop result if dropped outside a target', () => {
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+
+        backend.simulateBeginDrag(sourceHandle);
+        backend.simulateEndDrag();
+        expect(source.recordedDropResult).to.equal(false);
+      });
+
+      it('calls endDrag even if source was unregistered', () => {
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+
+        backend.simulateBeginDrag(sourceHandle);
+        registry.removeSource(sourceHandle);
+        expect(registry.getSource(sourceHandle)).to.equal(null);
+
+        backend.simulateEndDrag();
+        expect(source.recordedDropResult).to.equal(false);
+      });
+
+      it('throws in endDrag() if it is called outside a drag operation', () => {
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+        expect(() => backend.simulateEndDrag(sourceHandle)).to.throwError();
+      });
+
+      it('ignores drop() if no drop targets entered', () => {
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+
+        backend.simulateBeginDrag(sourceHandle);
+        backend.simulateDrop();
+        backend.simulateEndDrag();
+        expect(source.recordedDropResult).to.equal(false);
+      });
+
+      it('ignores drop() if drop targets entered and left', () => {
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+        const targetA = new NormalTarget();
+        const targetAHandle = registry.addTarget(Types.FOO, targetA);
+        const targetB = new NormalTarget();
+        const targetBHandle = registry.addTarget(Types.FOO, targetB);
+
+        backend.simulateBeginDrag(sourceHandle);
+        backend.simulateEnter(targetAHandle);
+        backend.simulateEnter(targetBHandle);
+        backend.simulateLeave(targetBHandle);
+        backend.simulateLeave(targetAHandle);
+        backend.simulateDrop();
+        backend.simulateEndDrag();
+        expect(targetA.didCallDrop).to.equal(false);
+        expect(targetB.didCallDrop).to.equal(false);
+        expect(source.recordedDropResult).to.equal(false);
+      });
+
+      it('ignores drop() if canDrop() returns false', () => {
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+        const target = new NonDroppableTarget();
+        const targetHandle = registry.addTarget(Types.FOO, target);
+
+        backend.simulateBeginDrag(sourceHandle);
+        backend.simulateEnter(targetHandle);
+        backend.simulateDrop();
+        expect(target.didCallDrop).to.equal(false);
+      });
+
+      it('ignores drop() if target has a different type', () => {
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+        const target = new NormalTarget();
+        const targetHandle = registry.addTarget(Types.BAR, target);
+
+        backend.simulateBeginDrag(sourceHandle);
+        backend.simulateEnter(targetHandle);
+        backend.simulateDrop();
+        expect(target.didCallDrop).to.equal(false);
+      });
+
+      it('throws in drop() if it is called outside a drag operation', () => {
+        expect(() => backend.simulateDrop()).to.throwError();
+      });
+
+      it('throws in drop() if it returns something that is neither undefined nor an object', () => {
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+        const target = new BadResultTarget();
+        const targetHandle = registry.addTarget(Types.FOO, target);
+
+        backend.simulateBeginDrag(sourceHandle);
+        backend.simulateEnter(targetHandle);
+        expect(() => backend.simulateDrop()).to.throwError();
+      });
     });
 
-    it('begins drag if canDrag() returns true', () => {
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
+    describe('enter() and leave()', () => {
+      it('throws in enter() if it is called outside a drag operation', () => {
+        const target = new NormalTarget();
+        const targetHandle = registry.addTarget(Types.BAR, target);
+        expect(() => backend.simulateEnter(targetHandle)).to.throwError();
+      });
 
-      backend.simulateBeginDrag(sourceHandle);
-      expect(source.didCallBeginDrag).to.equal(true);
-    });
+      it('throws in enter() if it is already in an entered target', () => {
+        const target = new NormalTarget();
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+        const targetHandle = registry.addTarget(Types.BAR, target);
 
-    it('throws in beginDrag() if it is called twice during one operation', () => {
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
+        backend.simulateBeginDrag(sourceHandle);
+        backend.simulateEnter(targetHandle);
+        expect(() => backend.simulateEnter(targetHandle)).to.throwError();
+      });
 
-      backend.simulateBeginDrag(sourceHandle);
-      expect(() => backend.simulateBeginDrag(sourceHandle)).to.throwError();
-    });
+      it('throws in leave() if it is called outside a drag operation', () => {
+        const target = new NormalTarget();
+        const targetHandle = registry.addTarget(Types.BAR, target);
+        expect(() => backend.simulateLeave(targetHandle)).to.throwError();
+      });
 
-    it('lets beginDrag() be called again in a next operation', () => {
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
+      it('throws in leave() if it is not entered', () => {
+        const target = new NormalTarget();
+        const source = new NormalSource();
+        const sourceHandle = registry.addSource(Types.FOO, source);
+        const targetHandle = registry.addTarget(Types.BAR, target);
 
-      backend.simulateBeginDrag(sourceHandle);
-      backend.simulateEndDrag(sourceHandle);
-
-      source.didCallBeginDrag = false;
-      expect(() => backend.simulateBeginDrag(sourceHandle)).to.not.throwError();
-      expect(source.didCallBeginDrag).to.equal(true);
-    });
-
-    it('endDrag() sees drop() return value as drop result if dropped on a target', () => {
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
-      const target = new NormalTarget();
-      const targetHandle = registry.addTarget(Types.FOO, target);
-
-      backend.simulateBeginDrag(sourceHandle);
-      backend.simulateEnter(targetHandle);
-      backend.simulateDrop();
-      backend.simulateEndDrag();
-      expect(target.didCallDrop).to.equal(true);
-      expect(source.recordedDropResult.foo).to.equal('bar');
-    });
-
-    it('endDrag() sees true as drop result by default if dropped on a target', () => {
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
-      const target = new TargetWithNoDropResult();
-      const targetHandle = registry.addTarget(Types.FOO, target);
-
-      backend.simulateBeginDrag(sourceHandle);
-      backend.simulateEnter(targetHandle);
-      backend.simulateDrop();
-      backend.simulateEndDrag();
-      expect(source.recordedDropResult).to.equal(true);
-    });
-
-    it('endDrag() sees false as drop result if dropped outside a target', () => {
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
-
-      backend.simulateBeginDrag(sourceHandle);
-      backend.simulateEndDrag();
-      expect(source.recordedDropResult).to.equal(false);
-    });
-
-    it('calls endDrag even if source was unregistered', () => {
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
-
-      backend.simulateBeginDrag(sourceHandle);
-      registry.removeSource(sourceHandle);
-      expect(registry.getSource(sourceHandle)).to.equal(null);
-
-      backend.simulateEndDrag();
-      expect(source.recordedDropResult).to.equal(false);
-    });
-
-    it('throws in endDrag() if it is called outside a drag operation', () => {
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
-      expect(() => backend.simulateEndDrag(sourceHandle)).to.throwError();
-    });
-
-    it('throws in enter() if it is called outside a drag operation', () => {
-      const target = new NormalTarget();
-      const targetHandle = registry.addTarget(Types.BAR, target);
-      expect(() => backend.simulateEnter(targetHandle)).to.throwError();
-    });
-
-    it('throws in enter() if it is already in an entered target', () => {
-      const target = new NormalTarget();
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
-      const targetHandle = registry.addTarget(Types.BAR, target);
-
-      backend.simulateBeginDrag(sourceHandle);
-      backend.simulateEnter(targetHandle);
-      expect(() => backend.simulateEnter(targetHandle)).to.throwError();
-    });
-
-    it('throws in leave() if it is called outside a drag operation', () => {
-      const target = new NormalTarget();
-      const targetHandle = registry.addTarget(Types.BAR, target);
-      expect(() => backend.simulateLeave(targetHandle)).to.throwError();
-    });
-
-    it('throws in leave() if it is not entered', () => {
-      const target = new NormalTarget();
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
-      const targetHandle = registry.addTarget(Types.BAR, target);
-
-      backend.simulateBeginDrag(sourceHandle);
-      expect(() => backend.simulateLeave(targetHandle)).to.throwError();
-    });
-
-    it('ignores drop() if no drop targets entered', () => {
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
-
-      backend.simulateBeginDrag(sourceHandle);
-      backend.simulateDrop();
-      backend.simulateEndDrag();
-      expect(source.recordedDropResult).to.equal(false);
-    });
-
-    it('ignores drop() if drop targets entered and left', () => {
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
-      const targetA = new NormalTarget();
-      const targetAHandle = registry.addTarget(Types.FOO, targetA);
-      const targetB = new NormalTarget();
-      const targetBHandle = registry.addTarget(Types.FOO, targetB);
-
-      backend.simulateBeginDrag(sourceHandle);
-      backend.simulateEnter(targetAHandle);
-      backend.simulateEnter(targetBHandle);
-      backend.simulateLeave(targetBHandle);
-      backend.simulateLeave(targetAHandle);
-      backend.simulateDrop();
-      backend.simulateEndDrag();
-      expect(targetA.didCallDrop).to.equal(false);
-      expect(targetB.didCallDrop).to.equal(false);
-      expect(source.recordedDropResult).to.equal(false);
-    });
-
-    it('ignores drop() if canDrop() returns false', () => {
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
-      const target = new NonDroppableTarget();
-      const targetHandle = registry.addTarget(Types.FOO, target);
-
-      backend.simulateBeginDrag(sourceHandle);
-      backend.simulateEnter(targetHandle);
-      backend.simulateDrop();
-      expect(target.didCallDrop).to.equal(false);
-    });
-
-    it('ignores drop() if target has a different type', () => {
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
-      const target = new NormalTarget();
-      const targetHandle = registry.addTarget(Types.BAR, target);
-
-      backend.simulateBeginDrag(sourceHandle);
-      backend.simulateEnter(targetHandle);
-      backend.simulateDrop();
-      expect(target.didCallDrop).to.equal(false);
-    });
-
-    it('throws in drop() if it is called outside a drag operation', () => {
-      expect(() => backend.simulateDrop()).to.throwError();
-    });
-
-    it('throws in drop() if it returns something that is neither undefined nor an object', () => {
-      const source = new NormalSource();
-      const sourceHandle = registry.addSource(Types.FOO, source);
-      const target = new BadResultTarget();
-      const targetHandle = registry.addTarget(Types.FOO, target);
-
-      backend.simulateBeginDrag(sourceHandle);
-      backend.simulateEnter(targetHandle);
-      expect(() => backend.simulateDrop()).to.throwError();
+        backend.simulateBeginDrag(sourceHandle);
+        expect(() => backend.simulateLeave(targetHandle)).to.throwError();
+      });
     });
   });
 });
